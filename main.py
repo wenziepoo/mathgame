930a1fa98b9e33498109705b517255724b256a59
wenziepoo <123613375+wenziepoo@users.noreply.github.com>
2023-01-25 19:53:20 -0800

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,20 +1,8 @@
-def mathgame():
-    global a, num1, ans, operations, operation, num2, string
-    a = 10
-    num1 = randint(0, a)
-    ans = randint(0, 30)
-    operations = ["x", "/", "+", "-"]
-    operation = operations[randint(0, 3)]
-    if operation == "x":
-        num2 = ans / num1
-    elif operation == "/":
-        num2 = num1/ans
-    elif operation == "+":
-        num2 = ans - num1
-    elif operation == "-":
-        num2 = -ans + num1
-    string = "" + str(num1) + operation + ("" + str(num2))
-    basic.show_string(string)
+def check():
+    global a
+    if input.button_is_pressed(Button.A) and input.button_is_pressed(Button.B):
+        a = 50
+def output():
     if input.button_is_pressed(Button.A):
         if operation == "x":
             op = num1 * num2
@@ -28,13 +16,39 @@ def mathgame():
         elif operation == "-":
             op = num1 - num2
             basic.show_string("" + str(op))
+        basic.pause(1000)
         mathgame()
+def mathgame():
+    global a, num1, ans, operations, operation, num2, string
+    a = 10
+    num1 = randint(1, a)
+    ans = randint(1, 50)
+    operations = ["x", "/", "+", "-"]
+    operation = operations[randint(0, 3)]
+    if operation == "x":
+        num2 = ans / num1
+        if num2 % 1 != 0:
+            mathgame()
+    elif operation == "/":
+        num2 = num1 / ans
+        if num2 % 1 != 0:
+            mathgame()
+    elif operation == "+":
+        num2 = ans - num1
+    elif operation == "-":
+        num2 = 0 - ans + num1
+    string = "  " + ("" + str(num1)) + operation + (" " + ("" + str(num2)))
+    basic.show_string("" + (string))
 string = ""
-num2 = 0
-operation = ""
 operations: List[str] = []
 ans = 0
+num2 = 0
 num1 = 0
+operation = ""
 a = 0
-basic.show_string(";)")
 mathgame()
+
+def on_forever():
+    check()
+    output()
+basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,49 +1,66 @@
-function mathgame() {
+function check () {
+    if (input.buttonIsPressed(Button.A) && input.buttonIsPressed(Button.B)) {
+        a = 50
+    }
+}
+function output () {
     let op: number;
-    
+if (input.buttonIsPressed(Button.A)) {
+        if (operation == "x") {
+            op = num1 * num2
+            basic.showString("" + op)
+        } else if (operation == "/") {
+            op = num1 / num2
+            basic.showString("" + op)
+        } else if (operation == "+") {
+            op = num1 + num2
+            basic.showString("" + op)
+        } else if (operation == "-") {
+            op = num1 - num2
+            basic.showString("" + op)
+        }
+        basic.pause(1000)
+        mathgame()
+    }
+}
+function mathgame () {
     a = 10
-    num1 = randint(0, a)
-    ans = randint(0, 30)
-    operations = ["x", "/", "+", "-"]
+    num1 = randint(1, a)
+    ans = randint(1, 50)
+    operations = [
+    "x",
+    "/",
+    "+",
+    "-"
+    ]
     operation = operations[randint(0, 3)]
     if (operation == "x") {
         num2 = ans / num1
+        if (num2 % 1 != 0) {
+            mathgame()
+        }
     } else if (operation == "/") {
         num2 = num1 / ans
+        if (num2 % 1 != 0) {
+            mathgame()
+        }
     } else if (operation == "+") {
         num2 = ans - num1
     } else if (operation == "-") {
-        num2 = -ans + num1
+        num2 = 0 - ans + num1
     }
-    
-    string = "" + ("" + num1) + operation + ("" + ("" + num2))
-    basic.showString(string)
-    if (input.buttonIsPressed(Button.A)) {
-        if (operation == "x") {
-            op = num1 * num2
-            basic.showString("" + ("" + op))
-        } else if (operation == "/") {
-            op = num1 / num2
-            basic.showString("" + ("" + op))
-        } else if (operation == "+") {
-            op = num1 + num2
-            basic.showString("" + ("" + op))
-        } else if (operation == "-") {
-            op = num1 - num2
-            basic.showString("" + ("" + op))
-        }
-        
-        mathgame()
-    }
-    
+    string = "  " + ("" + num1) + operation + (" " + ("" + num2))
+    basic.showString("" + (string))
 }
-
 let string = ""
-let num2 = 0
-let operation = ""
-let operations : string[] = []
+let operations: string[] = []
 let ans = 0
+let num2 = 0
 let num1 = 0
+let operation = ""
 let a = 0
-basic.showString(";)")
 mathgame()
+basic.forever(function () {
+    check()
+    output()
+})
